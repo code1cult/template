c37acf49070c3866f235772e389c94b5605a1e98
ilovetraffic <aidarfreelancer@gmail.com>
2018-11-04 05:05:11 +0300

    add

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,37 @@
 import React from 'react'
 import Helmet from 'react-helmet';
 
+
 const Header = (data) => {
     return (
         <div>
+
             <Helmet>
+                <script
+                    dangerouslySetInnerHTML={{
+                        __html: `
+                        <script id="spout-tag-81b4aa5e-1b17-49c6-be5c-def049a19eb2">
+                        (function() {
+                          var r = encodeURIComponent(top.document.referrer.substring(0,250)),
+                              p = encodeURIComponent(top.document.location.href.substring(0,250)),
+                              t = Date.now(),
+                              u = '81b4aa5e-1b17-49c6-be5c-def049a19eb2',
+                              e = document.createElement('script'),
+                              s = sessionStorage.getItem('spoutable-' + u);
+                          if (!s) {
+                            var m = Math.random.bind(Math);
+                            s = JSON.stringify({ sessionId: [ t, m(), m(), m(), m(), m(), m(), m(), m(), m(), m(), m(), m() ]});
+                            sessionStorage.setItem('spoutable-' + u, s);
+                          }
+                          e.async = true;
+                          e.src='//s.spoutable.com/s?u='+u+'&s='+encodeURIComponent(s)+'&t='+t+'&r='+r+'&p='+p;
+                          document.head.appendChild(e);
+                        })();
+                      </script>
+                      
+        `
+                    }}
+                />
                 <title>{data.title}</title>
                 <meta name="description" content={data.description} />
             </Helmet>
